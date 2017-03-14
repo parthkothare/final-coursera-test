e953b48fb1be4483f4560cb066cacadb589c8240
parthkothare <parthkothare@gmail.com>
2017-03-14 14:58:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-// should be undefined
-var x;
-console.log(x);
+/ Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("name.txt", 
+            function (request) {
+              var name = request.responseText;
 
-if (x == undefined) {
-  console.log("x is undefined");
-}
+              document.querySelector("#content")
+                .innerHTML = "<h2>Hello " + name + "!</h2>";
+            });
 
-x = 5;
-if (x == undefined) {
-  console.log("x is undefined");
-}
-else {
-  console.log("x has been defined");
-}
+        
+      });
+  }
+);
+
